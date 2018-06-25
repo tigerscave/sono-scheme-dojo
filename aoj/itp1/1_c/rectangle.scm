92c041eafd47b1efd00fdbc47554eed6a27e8d62
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-06-25 09:43:00 +0900

    refactoring: add reftangle

--- aoj/itp1/1_c/rectangle.scm
+++ aoj/itp1/1_c/rectangle.scm
@@ -1,1 +1,37 @@
-(print "hello rectangle")
+(define first
+  (lambda (tup)
+    (* (car tup) (car (cdr tup)))))
+
+(define second
+  (lambda (tup)
+    (* (plus (car tup) (car (cdr tup))) 2)))
+
+(define rectangle
+  (lambda (tup)
+    (cond
+      ((null? tup) '())
+      (else
+        (cons (* (car tup) (car (cdr tup)))
+          (cons (* (+ (car tup) (car (cdr tup))) 2) '())
+            )))))
+
+(define first
+  (lambda (tup)
+    (car tup)))
+
+(define second
+  (lambda (tup)
+    (car (cdr tup))))
+
+(define rectangle2
+  (lambda (tup)
+    (cons (* (first tup) (second tup))
+      (cons (* 2 (+ (first tup) (second tup))) '())
+        )))
+
+(print (first '(3 5)))
+(print (second '(3 5)))
+(print "------(rectangle '(3 5))-------")
+(print (rectangle '(3 5)))
+(print "------(rectangle2 '(10 20))-------")
+(print (rectangle2 '(10 20)))
