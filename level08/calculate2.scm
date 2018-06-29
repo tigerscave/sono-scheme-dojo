b7020ccf900076caff9962db0cb57c21b668f13d
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-06-30 08:55:04 +0900

    temp: I can't make currying function of calculate

--- level08/calculate2.scm
+++ level08/calculate2.scm
@@ -3,6 +3,10 @@
     (lambda (n1 n2)
       (cal n1 n2))))
 
-(define calculate2 ((calculate cal) n1 n2))
+(define calculate2
+  (lambda (cal)
+    (lambda (n1)
+      (lambda (n2)
+       (calculate cal n1 n2)))))
 
-(print ((calculate2 +) 4 5)) 
+(print (((calculate2 +) 4) 5))
