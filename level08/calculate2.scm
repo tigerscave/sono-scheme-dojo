db3d0c10318faa7b0458b0ca51c7e5d670c1b67c
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-06-30 11:38:23 +0900

    chore: add calculate2 and pow

--- level08/calculate2.scm
+++ level08/calculate2.scm
@@ -7,6 +7,17 @@
   (lambda (cal)
     (lambda (n1)
       (lambda (n2)
-       (calculate cal n1 n2)))))
+       ((calculate cal) n1 n2)))))
 
-(print (((calculate2 +) 4) 5))
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+(define pow
+  (lambda (n1 n2)
+    (cond
+      ((zero? n2) 1)
+      (else
+        (* n1 (pow n1 (sub1 n2)))))))
+
+(print  (((calculate2 pow) 4) 5))
