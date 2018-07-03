81685f87b0515646ee3667948cf3345b4aeb258b
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-04 08:56:24 +0900

    chore: add looking function

---
+++ level09/looking.scm
@@ -0,0 +1,23 @@
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define keep-looking
+  (lambda (a sorn lat)
+    (cond
+      ((number? sorn)
+        (keep-looking a (pick sorn lat) lat))
+      (else (eq? sorn a)))))
+
+(define looking
+  (lambda (a lat)
+    (keep-looking a (pick 1 lat) lat)))
+
+(print (looking 'cavior '(6 2 4 cavior 5 7 3)))
+(print (looking 'cavior '(6 2 grits cavior 5 7 3)))
