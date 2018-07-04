f90f837184ec16d3594f91697a36113ed5ace139
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-05 08:33:53 +0900

    add collatz

---
+++ level09/collatz.scm
@@ -0,0 +1,19 @@
+(define add1
+  (lambda (x)
+    (+ x 1)))
+
+(define one?
+  (lambda (x)
+    (= x 1)))
+
+(define collatz
+  (lambda (x)
+    (cond
+      ((one? x) 1)
+      (else
+        (cond
+          ((even? x) (collatz (/ x 2)))
+          (else (collatz (add1 (* 3 x)))))))))
+
+(print (collatz 5))
+(print (collatz 9948903289208423840948320980938432))
