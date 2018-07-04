baca7ba82d6d1b2708364eb40ee8a57243d5e943
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-04 15:04:44 +0900

    chore: add length*, weight*, shuffle

--- level09/align.scm
+++ level09/align.scm
@@ -38,3 +38,52 @@
       (else
         (build (first para)
           (align (second para)))))))
+
+(define length*
+  (lambda (para)
+    (cond
+      ((atom? para) 1)
+      (else
+        (+ (length* (first para))
+            (length* (second para)))))))
+
+(print (length* '((a b) c)))
+
+
+(define weight*
+  (lambda (para)
+    (cond
+      ((atom? para) 1)
+      (else
+        (+ (* (weight* (first para)) 2)
+            (weight* (second para)))))))
+
+(print (weight* '((a b) c)))
+
+
+(define revpair
+  (lambda (para)
+    (build (second para) (first para))))
+
+(print (revpair '((a b) (c d))))
+
+(define a-pair?
+  (lambda (x)
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      ((null? (cdr (cdr x))) #t)
+      (else #f))))
+
+(define shuffle
+  (lambda (para)
+    (cond
+      ((atom? para) para)
+      ((a-pair? (first para))
+        (shuffle (revpair para)))
+      (else
+        (build (first para)
+                  (shuffle (second para)))))))
+
+(print (shuffle '((a b) (c d))))
