801e8139aa02d9b2413c757e55c8e1973258526d
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-05 21:18:24 +0900

    chore: add acckermann

---
+++ level09/acckermann.scm
@@ -0,0 +1,18 @@
+(define sub1
+  (lambda (x)
+    (- x 1)))
+
+(define add1
+  (lambda (x)
+    (+ x 1)))
+
+(define acckermann
+  (lambda (n m)
+    (cond
+      ((zero? n) (add1 m))
+      ((zero? m) (acckermann (sub1 n) 1))
+      (else
+        (acckermann (sub1 n)
+          (acckermann n (sub1 m)))))))
+
+(print (acckermann 1 1))
