b02ecc55d23b6b2017cc9a328587f40f9eddcb34
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-07 13:44:30 +0900

    temp: add mk-length

--- level09/mk-length.scm
+++ level09/mk-length.scm
@@ -52,3 +52,22 @@
       eternity)))
   '(a b))
 )
+
+(print "-----length100------")
+(print
+  (
+    ((lambda (le)
+      ((lambda (mk-length)
+        (mk-length mk-length))
+      (lambda (mk-length)
+        (le (lambda (x)
+              ((mk-length mk-length) x))))))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+  '(a b c d e f g f i o kokoko ooooo))
+)
+
+(print le)
