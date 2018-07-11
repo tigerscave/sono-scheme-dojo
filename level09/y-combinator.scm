2a8f7f0defd30982ae321573d66427aa5cb551b7
Koki Sonoda <m.g.20spurs1304@gmail.com>
2018-07-11 11:25:08 +0900

    add length process

--- level09/y-combinator.scm
+++ level09/y-combinator.scm
@@ -65,3 +65,222 @@
     '(a b)
   )
 )
+
+(print "-----length0-----")
+(print
+  (
+    ((lambda (length)
+      (lambda (l)
+        (cond
+          ((null? l) 0)
+          (else (add1 (length (cdr l)))))))
+      eternity)
+    '()
+  )
+)
+
+(print "-----length1-----")
+(print
+  (
+    ((lambda (length)
+      (lambda (l)
+        (cond
+          ((null? l) 0)
+          (else (add1 (length (cdr l)))))))
+      ((lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l)))))))
+      eternity))
+    '(a)
+  )
+)
+
+(print "-----length2-----")
+(print
+  (
+    ((lambda (length)
+      (lambda (l)
+        (cond
+          ((null? l) 0)
+          (else (add1 (length (cdr l)))))))
+      ((lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l)))))))
+        ((lambda (length)
+          (lambda (l)
+            (cond
+              ((null? l) 0)
+              (else (add1 (length (cdr l)))))))
+          eternity)))
+    '(a b)
+  )
+)
+
+(print "-----length0-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length eternity))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+    '()
+  )
+)
+
+(print "-----length1-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length
+        (mk-length eternity)))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+    '(a)
+  )
+)
+
+(print "-----length2-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length
+        (mk-length
+          (mk-length eternity))))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+    '(a b)
+  )
+)
+
+(print "-----length3-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length
+        (mk-length
+          (mk-length
+            (mk-length eternity)))))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+    '(a b a)
+  )
+)
+
+(print "-----length0-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length mk-length))
+      (lambda (mk-length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (mk-length (cdr l))))))))
+    '()
+  )
+)
+
+
+(print "-----length1-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length mk-length))
+      (lambda (mk-length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1
+              ((mk-length eternity) (cdr l))))))))
+    '(s)
+  )
+)
+
+
+(print "-----length-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length mk-length))
+      (lambda (mk-length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1
+              ((mk-length mk-length) (cdr l))))))))
+    '(s a a)
+  )
+)
+
+(print "-----length-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length mk-length))
+      (lambda (mk-length)
+        ((lambda (length)
+          (lambda (l)
+            (cond
+              ((null? l) 0)
+              (else (add1
+                (length (cdr l)))))))
+        (lambda (x)
+          ((mk-length mk-length) x)))))
+    '(s a a)
+  )
+)
+
+
+(print "-----length-----")
+(print
+  (
+    ((lambda (mk-length)
+      (mk-length mk-length))
+      (lambda (mk-length)
+        ((lambda (length)
+          (lambda (l)
+            (cond
+              ((null? l) 0)
+              (else (add1
+                (length (cdr l)))))))
+        (lambda (x)
+          ((mk-length mk-length) x)))))
+    '(s a a)
+  )
+)
+
+
+(print "-----length-----")
+(print
+  (
+    ((lambda (le)
+      ((lambda (mk-length)
+        (mk-length mk-length))
+        (lambda (mk-length)
+          (le
+            (lambda (x)
+              ((mk-length mk-length) x))))))
+      (lambda (length)
+        (lambda (l)
+          (cond
+            ((null? l) 0)
+            (else (add1 (length (cdr l))))))))
+      '(s a a a)
+  )
+)
